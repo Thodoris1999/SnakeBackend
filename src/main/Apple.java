9aeb37e86f5417ca4fd1b3a27394b079f6871a20
oogee <teotyrov@gmail.com>
2018-11-05 19:17:28 +0200

    add Apple class

---
+++ src/main/Apple.java
@@ -0,0 +1,55 @@
+package main;
+
+public class Apple {
+	
+	private int appleId;
+	private int appleTileId;
+	private String color;
+	private int points;
+	
+	public Apple(int appleId, int appleTileId, String color, int points) {
+		this.appleId = appleId;
+		this.appleTileId = appleTileId;
+		this.color = color;
+		this.points = points;
+	}
+	
+	public Apple(Apple apple) {
+		this.appleId = apple.getAppleId();
+		this.appleTileId = apple.getAppleTileId();
+		this.color = apple.getColor();
+		this.points = apple.getPoints();
+	}
+
+	public int getAppleId() {
+		return appleId;
+	}
+
+	public void setAppleId(int appleId) {
+		this.appleId = appleId;
+	}
+
+	public int getAppleTileId() {
+		return appleTileId;
+	}
+
+	public void setAppleTileId(int appleTileId) {
+		this.appleTileId = appleTileId;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+}
