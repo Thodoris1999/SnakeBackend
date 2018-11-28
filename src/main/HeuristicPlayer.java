0631c3938c858c2af90f2fd91969d65a47e8f748
oogee <teotyrov@gmail.com>
2018-11-28 12:44:16 +0200

    small adjustments and comments on HeuristicPlayer

--- src/main/HeuristicPlayer.java
+++ src/main/HeuristicPlayer.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents player in snake that moves accordingly to the best possible move,
+ * maximizing the value returned by {@link #evaluate(int, int)}
+ * Also supports printing some basic statistics about the turn history of the player.
+ * 
+ * @author Τυροβούζης Θεόδωρος 
+ * AEM 9369 
+ * phone number 6955253435 
+ * email ttyrovou@ece.auth.gr
+ * 
+ * @author Τσιμρόγλου Στυλιανός 
+ * AEM 9468 
+ * phone number 6977030504 
+ * email stsimrog@ece.auth.gr
+ */
 public class HeuristicPlayer extends Player {
 
     private ArrayList<int[]> path;
@@ -22,11 +37,19 @@ public class HeuristicPlayer extends Player {
         int oldScore = score;
         int[] move = move(currentPos, dice, true);
         int newScore = score;
-        // undo score changes
+        // undo score changes because we are just evaluating
         score = oldScore;
         return 0.8 * (move[0] - currentPos) + 0.2 * (newScore - oldScore);
     }
 
+    /**
+     * Calculates the best possible move in the player's current position, maximizing the value returned by
+     * {@link #evaluate(int, int)}, then executes that move and finally, it updates the {@link #path} Arraylist
+     * according to the selected move.
+     * 
+     * @param currentPos The position of the heuristic player in the board
+     * @return The position of the heuristic player after their turn is complete
+     */
     public int getNextMove(int currentPos) {
         Map<Integer, Double> moves = new HashMap<>();
 
@@ -37,7 +60,7 @@ public class HeuristicPlayer extends Player {
         for (int i = 2; i < 7; i++) {
             double reward = evaluate(currentPos, i);
             moves.put(i, reward);
-            if (reward > highestRewardDie)
+            if (reward > moves.get(highestRewardDie))
                 highestRewardDie = i;
         }
 
@@ -47,18 +70,46 @@ public class HeuristicPlayer extends Player {
         return move[0];
     }
 
+    /**
+     * Prints the:
+     * <ul>
+     *   <li>the die that was selected</li>
+     *   <li>number of ladders climbed</li>
+     *   <li>number snake heads that were stepped on</li>
+     *   <li>number of red apples eaten</li>
+     *   <li>number of black apples eaten</li>
+     * </ul>
+	 * for each turn the heuristic player has played
+     */
     public void statistics() {
         for (int i = 0; i < path.size(); i++) {
-            System.out.println("turn " + (i + 1) + ": Stepped on " + path.get(i)[1] + " snake heads, climbed " +
-            path.get(i)[2] + " ladders, ate " + path.get(i)[3] + " red apples and " + path.get(i)[4] + " black apples");
-
+        	int die;
+        	if (i == 0)
+        		die = path.get(i)[0];
+        	else
+        		die = path.get(i)[0] - path.get(i -1)[0];
+        	
+            System.out.println("turn " + (i + 1) + " (selected die=)" + die + ": Stepped on " + path.get(i)[1] +
+            		" snake heads, climbed " + path.get(i)[2] + " ladders, ate " + path.get(i)[3] +
+            		" red apples and " + path.get(i)[4] + " black apples");
+            
         }
     }
 
+    /**
+     * Returns an ArrayList of information on each turn the player has played
+     * 
+     * @return an ArrayList of information on each turn the player has played
+     */
     public ArrayList<int[]> getPath() {
         return path;
     }
 
+    /**
+     * Specify an ArrayList of information on each turn the player has played
+     * 
+     * @param path an ArrayList of information on each turn the player has played
+     */
     public void setPath(ArrayList<int[]> path) {
         this.path = path;
     }
